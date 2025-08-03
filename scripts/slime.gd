2272467a8e7973c40f80c2895db72f3d9ad5537f
sisuisthebest <109489645+kirakai-dev@users.noreply.github.com>
2025-08-03 14:51:37 -0600

    Slime dies
    Added ability to kill enemies

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -7,15 +7,23 @@ const speed = 60
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_up: RayCast2D = $RayCastUp
+@onready var timer: Timer = $Timer
+@onready var collision_shape_2d: CollisionShape2D = $"Kill Zone/CollisionShape2D"
+
 
 
 func _process(delta):
 	
-	if ray_cast_right.is_colliding():	
+	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = false
-	
 	position.x += direction * speed * delta
+	if ray_cast_up.is_colliding():
+		queue_free()
+
+func _on_timer_timeout() -> void:
+	queue_free()
