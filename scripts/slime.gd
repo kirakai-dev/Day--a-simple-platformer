caec19b5a09be94f1331e3e012af59a25d60f9a5
sisuisthebest <109489645+kirakai-dev@users.noreply.github.com>
2025-08-16 13:07:04 -0600

    Level 2
    Started work on Level 2.

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -6,10 +6,11 @@ const speed = 60
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_up: RayCast2D = $RayCastUp
 @onready var timer: Timer = $Timer
 @onready var collision_shape_2d: CollisionShape2D = $"Kill Zone/CollisionShape2D"
+
 
 
 
@@ -17,15 +18,13 @@ func _process(delta):
 	
 	if ray_cast_right.is_colliding():
 		direction = -1
-		animated_sprite.flip_h = true
+		sprite.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = false
+		sprite.flip_h = false
 	position.x += direction * speed * delta
 	if ray_cast_up.is_colliding():
-		print("+1 Kills")
 		kills += 1
-		print(kills)
 		queue_free()
 
 func _on_timer_timeout() -> void:
